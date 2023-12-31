6eb7e76035c32ae21ec0d30ac19ae301e6c1a017
kienhoa <giaumavip@gmail.com>
2023-12-31 10:21:59 +0700

    fix bug

--- main.js
+++ main.js
@@ -46,7 +46,7 @@ for (var i =0; i < menuItems.length; i++) {
 // tự động đóng menu khi chọn (pc) 
 var subMenu = document.getElementById('Menu-subnav')
 subMenu.onclick = function(e) {
-    if(e.style.display === 'none') {
-        e.style.display === block
-    }else{e.style.display === 'none' }
+    if(e.target.style.display === 'none') {
+        e.target.style.display === block
+    }else{e.target.style.display === 'none' }
 }
